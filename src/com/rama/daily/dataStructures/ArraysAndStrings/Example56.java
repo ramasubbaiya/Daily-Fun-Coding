0f1294a71ed0d302e5c471bd6506c2bb36c679e5
Billy Everyteen <ramasubbaiya@gmail.com>
2016-08-03 18:33:49 -0400

    odd numbers

--- src/com/rama/daily/dataStructures/ArraysAndStrings/Example56.java
+++ src/com/rama/daily/dataStructures/ArraysAndStrings/Example56.java
@@ -4,17 +4,21 @@ public class Example56 {
 
 	public static void occurenceOfString() {
 		String str = "aaabbbcccas";
-		
-		for(int i = 0; i < str.length(); i++) {
+		int strLen = str.length();
+		// boolean[] isChecked = new boolean[124];
+		for(int i = 0; i < strLen; i++) {
 			char c = str.charAt(i);
 			char[] ch = str.toCharArray();
 			int count = 0;
 			//char[] checkChar = 
-			for(int j = 0; j < str.length(); j++) {
+			for(int j = 0; j < strLen; j++) {
+				//int chNum = ch[j];
 				if(ch[j] == c) {
+					
 					count++;
 				}
 			}
+			//isChecked[chNum] = true;
 			System.out.println("The character : " +c);
 			System.out.println("Counter : " + count);
 		}
--- src/com/rama/daily/dataStructures/ArraysAndStrings/Example57.java
+++ src/com/rama/daily/dataStructures/ArraysAndStrings/Example57.java
@@ -1,8 +1,20 @@
 package com.rama.daily.dataStructures.ArraysAndStrings;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Example57 {
 
-	
+	public List<Integer> findOddNumbers(List<Integer> nums) {
+		List<Integer> oddNums = new ArrayList<Integer>();
+		for(int num: nums) {
+			if(num%2 != 0) {
+				oddNums.add(num);
+			}
+		}	
+		return oddNums;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
